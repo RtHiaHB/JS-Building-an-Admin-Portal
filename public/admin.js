96bec6979b23ea8ff4eca6d2bb4885784ed8a0ab
Thomas Davidson <tomd1969@gmail.com>
2022-10-23 15:34:59 -0400

    completed assignment.  I got it working!  YAY! :)

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,48 @@
 
 // Your Code Here
+async function main() {
+    let response = await fetch("http://localhost:3001/listBooks");
+    let books = await response.json();
+    for(let i = 0; i< books.length; i++) {
+        adminListBooks(books[i]);
+    }
+}
+
+function adminListBooks(book) {
+    console.log('Listing books...');
+    let rootDiv = document.getElementById('root');
+    let newDiv = document.createElement('div');
+    newDiv.display = 'flex';
+    let newTextbox = document.createElement('input');
+    newTextbox.value = book.quantity;
+    newTextbox.id = 'book' + book.id;
+    let newLabel = document.createElement('label');
+    newLabel.textContent = book.title;
+    newLabel.htmlFor = 'book' + book.id;
+    let newButton = document.createElement('button');
+    newButton.textContent = 'Save'
+    newButton.id = 'button' + book.id;
+    newButton.addEventListener('click', (e) => {
+        SaveBookQty(book, newTextbox.value);
+    })
+    newDiv.append(newLabel);
+    newDiv.append(newTextbox);
+    newDiv.append(newButton);
+    rootDiv.append(newDiv);
+}
+
+async function SaveBookQty(book, newQty) {
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            id: book.id,
+            quantity: newQty
+        })
+
+    })
+}
+
+main();
